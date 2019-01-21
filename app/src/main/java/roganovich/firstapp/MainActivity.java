4ee92272348325d33986b0e938cea7dfc4985532
Roman Roganovich <roman.roganovich@mail.ru>
2019-01-21 18:52:45 +0300

    animation

--- app/src/main/java/roganovich/firstapp/MainActivity.java
+++ app/src/main/java/roganovich/firstapp/MainActivity.java
@@ -7,37 +7,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.ContextMenu;
-import android.view.Gravity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    SeekBar sBar;
-
-    TextView txtName;
-    RadioButton rbLeft;
-    RadioButton rbCenter;
-    RadioButton rbRight;
-    RadioGroup  rdbGroup;
-    Button btnCreate;
-    Button btnClear;
-    LinearLayout lResult;
-
-    LinearLayout.LayoutParams lParams1;
-    LinearLayout.LayoutParams lParams2;
-
-    int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
+    TextView txt1;
 
 
     @Override
@@ -45,88 +28,42 @@ public class MainActivity extends Activity implements View.OnClickListener, Seek
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        sBar = (SeekBar) findViewById(R.id.sBar);
-        sBar.setOnSeekBarChangeListener(this);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        txt1 = (TextView) findViewById(R.id.txt1);
 
-        txtName = (TextView) findViewById(R.id.txtName);
-        rdbGroup = (RadioGroup) findViewById(R.id.rdbGroup);
-
-        btnCreate = (Button) findViewById(R.id.btnCreate);
-        btnCreate.setOnClickListener(this);
-
-        btnClear = (Button) findViewById(R.id.btnClear);
-        btnClear.setOnClickListener(this);
-
-        lResult = (LinearLayout) findViewById(R.id.loResult);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
 
 
-        lParams1 = (LinearLayout.LayoutParams) btnCreate.getLayoutParams();
-        lParams2 = (LinearLayout.LayoutParams) btnCreate.getLayoutParams();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
+        Animation anim = null;
         switch (v.getId()){
-            case R.id.btnCreate:
-                LinearLayout.LayoutParams lParms = new LinearLayout.LayoutParams(wrapContent,wrapContent);
-
-                int btnGravity = Gravity.LEFT;
-                switch (rdbGroup.getCheckedRadioButtonId()){
-                    case R.id.rbLeft:
-                        btnGravity = Gravity.LEFT;
-                        break;
-                    case R.id.rbCenter:
-                        btnGravity = Gravity.CENTER;
-                        break;
-                    case R.id.rbRight:
-                        btnGravity = Gravity.RIGHT;
-                        break;
-                }
-
-                lParms.gravity = btnGravity;
-
-                Button newButton = new Button(this);
-                newButton.setText(txtName.getText().toString());
-                lResult.addView(newButton,lParms);
+            case R.id.btn1:
+                anim = AnimationUtils.loadAnimation(this,R.anim.myalpha);
                 break;
-
-            case R.id.btnClear:
-                lResult.removeAllViews();
-                Toast.makeText(this, "Удалено", Toast.LENGTH_LONG).show();
+            case R.id.btn2:
+                anim = AnimationUtils.loadAnimation(this,R.anim.mycombo);
+                break;
+            case R.id.btn3:
+                anim = AnimationUtils.loadAnimation(this,R.anim.myrotate);
+                break;
+            case R.id.btn4:
+                anim = AnimationUtils.loadAnimation(this,R.anim.myscale);
+                break;
+            case R.id.btn5:
+                anim = AnimationUtils.loadAnimation(this,R.anim.mytrans);
                 break;
         }
-    }
-
-
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        int rightProgress = progress;
-        int leftProgress = seekBar.getMax() - progress;
-
-        lParams1.weight = leftProgress;
-        lParams2.weight = rightProgress;
-
-        btnCreate.setText(String.valueOf(rightProgress));
-        btnClear.setText(String.valueOf(leftProgress));
-
-    }
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-
+        txt1.startAnimation(anim);
     }
 }
